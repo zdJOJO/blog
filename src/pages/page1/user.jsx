a47605ba4343a5fe1800552325cd892f333e3201
zdJOJO <461863156@qq.com>
2017-08-24 18:41:51 +0800

    发现问题，ExtractTextPlugin不能提取出单独的css文件

--- src/pages/page1/user.jsx
+++ src/pages/page1/user.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import "./user.less";
 
 const User = ({match}) => {
   return(
     <div>
-      <h5>{match.params.userId}</h5>
+      <h5 className="userName">我的ID: {match.params.userId}</h5>
     </div>
   );
 };

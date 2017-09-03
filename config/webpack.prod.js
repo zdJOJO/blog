23584e9c56527d04296a1c386703a28ca316ad49
zdJOJO <461863156@qq.com>
2017-09-04 00:34:25 +0800

    bug fix; 把项目放到了服务器上。。 node依赖包不能直接用 npm isntall 。。  需要自己手动安装。。。

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -68,7 +68,9 @@ module.exports = {
   plugins: [
 
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "production")
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
     }),
 
     new webpack.optimize.UglifyJsPlugin({

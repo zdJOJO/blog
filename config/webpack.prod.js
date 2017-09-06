cea50f9fc66dc0555712fe016b11b30b1d21013a
zdJOJO <461863156@qq.com>
2017-09-06 14:10:38 +0800

    add optimize-css-assets-webpack-plugin

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,6 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 
 if (process.env.NODE_ENV !== "production") {
@@ -45,7 +46,10 @@ module.exports = {
           fallback: "style-loader",
           use: [
             {
-              loader: "css-loader"
+              loader: "css-loader",
+              options: {
+                minimize: true
+              }
             }
           ]
         })
@@ -88,9 +92,13 @@ module.exports = {
     }),
 
     new webpack.optimize.UglifyJsPlugin({
+      beautify: false,  // 最紧凑的输出
       comments: false,        //去掉注释
       compress: {
-        warnings: false  //忽略警告,要不然会有一大堆的黄色字体出现
+        warnings: false,  //忽略警告,要不然会有一大堆的黄色字体出现
+        drop_console: true, // 删除所有的 `console` 语句
+        collapse_vars: true, // 内嵌定义了但是只用到一次的变量
+        reduce_vars: true // 提取出出现多次但是没有定义成变量去引用的静态值
       },
       except: ["$super", "$", "exports", "require"]    //排除关键字
     }),
@@ -105,6 +113,10 @@ module.exports = {
       filename: "[name].[contenthash].css", 
       disable: false, 
       allChunks: true 
+    }),
+
+    new OptimizeCssAssetsPlugin({
+      cssProcessorOptions: { discardComments: {removeAll: true }}
     }),
     
     new webpack.NoEmitOnErrorsPlugin()
